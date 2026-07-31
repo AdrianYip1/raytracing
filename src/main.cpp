6e86c7cab61b0e581214d2fa8dca73f6b2024683
Adrian Yip <yipadrian1119@gmail.com>
2026-07-30 19:30:23 -0600

    feat: changed from ppm to png files via stb image library

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "vendor/stb_image_write.h"
+#include <cstdlib>
+#include <vector>
 
 int main() {
 	// Image
 	int imageW = 256;
 	int imageH = 256;
+	std::vector<unsigned char> data;
+	data.reserve(imageW * imageH * 3);
 
 	// Render
-	std::cout << "P3\n" << imageW << ' ' << imageH << "\n255\n";
-
 	for (int j = 0; j < imageH; j++) {
 		for (int i = 0; i < imageW; i++) {
 			auto r = double(i) / (imageW - 1);
@@ -18,8 +22,13 @@ int main() {
 			int ig = int(255.999 * g);
 			int ib = int(255.999 * b);
 
-			std::cout << ir << ' ' << ig << ' ' << ib << ' ' << '\n';
+			data.push_back(static_cast<unsigned char>(ir));
+			data.push_back(static_cast<unsigned char>(ig));
+			data.push_back(static_cast<unsigned char>(ib));
 		}
 	}
+
+	stbi_write_png("image.png", imageW, imageH, 3, data.data(), imageW * 3);
+	system("start image.png");
 	return 0;
 }
